8a40b44a86f9859e2a3c1fc00f2cf820a2fa04fb
Cody <moxy@outlook.com.au>
2018-10-16 10:51:20 +0000

    Update titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  # Return title depending on page
+  def get_page_title(page_title = "")
+    base_title = "Prosolv"
+    if page_title.empty?
+      base_title
+    else
+       page_title + " | " + base_title
+    end
+  end
 end
